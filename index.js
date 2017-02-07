5d268923a3b1cb65fb24f710de45682de072b93f
Rob Hitt <robhitt@gmail.com>
2017-02-06 22:09:08 -0500

    add functionality for ability to listen to albums

--- index.js
+++ index.js
@@ -2,41 +2,86 @@ $(document).ready(addFormEventHandler)
 
 
 function addFormEventHandler (){
-  $('form#song-form').submit(handleFormSubmit)
+  $('form#album-form').submit(handleFormSubmit)
 }
 
 function handleFormSubmit (event){
   console.log(event)
   event.preventDefault()
-  findAndRenderSongs()
+  findAndRenderAlbums()
 }
 
-function findAndRenderSongs(){
-  const URL = 'https://api.spotify.com/v1/artists/'
-  // find the user's search query and interpolate that into the URL
+function findAndRenderAlbums(){
+  const BEGURL = 'https://api.spotify.com/v1/search?q=artist:'
+  const ENDURL = '&type=album'
+
   let $input = $('input#query')
   let userInput = $input.val()
   let query = userInput.split(' ').join('+')
   $input.val('')
-  // 1. Find what the user put in the input field and then append that into the url as so
-  // 'https://api.spotify.com/v1/artists/{id}'
-  // 1. Fire an XHR reqeust to the Spotify API
+  var renderSomething = renderAlbums.bind(null, query)
+  // debugger;
 
   $.ajax({
-    url: `${URL}?q=${query}`,
-    success: renderSongs
+    url: BEGURL + query + ENDURL,
+
+    success: renderSomething // success passes into data below function
   })
 }
 
-function renderSongs (data){
-  // 2. When the response comes back, append some lis to my ul for the user
-  let songList = $('.js--song-list')
-  songList.html('')
+function renderAlbums (query, data) {
 
-  function renderSongs ( song ) {
-    let title = song.volumeInfo.title
-    songList.append(`<li class='collection-item'>${title}</li>`)
+  let albumList = $('ul.js--album-list')
+  let artistName = $('div#js--artist-name')
+
+  albumList.html('')
+  artistName.html('')
+  let temp_artist_name = capitalize(query.split('+').join(' '))
+  artistName.prepend(`${temp_artist_name}`)
+
+  function renderAlbum ( album ) {
+
+    let albumName
+    let albumLink
+    let albumImage
+
+    if (query.split('+').join(' ').toLowerCase() == album.artists[0].name.toLowerCase()) {
+
+      albumName = album.name
+      albumLink = album.external_urls.spotify
+      albumImage = album.images[0].url
+      albumEmbed = album.uri
+
+
+
+      albumList.append(`<li class='collection-item'>
+      <a href="${albumLink}" target="_blank"><h4>${albumName}</h4></a>
+      <br>
+      <a href="${albumLink}" target="_blank"><img src="${albumImage}" width="600" height="600">
+      <br>
+      <iframe src="https://embed.spotify.com/?uri=${albumEmbed}" width="600" height="380" frameborder="0" allowtransparency="true"></iframe>
+
+      </li>`)
+    }
+
+
+
+
   }
 
-  data.items.forEach(renderSongs)
+  data.albums.items.forEach(renderAlbum)
+
 }
+
+function capitalize (sentence) {
+  var words = sentence.split(" ")
+  var newSentence = []
+  words.forEach(function(element){
+    newSentence.push(element[0].toUpperCase() + element.slice(1))
+  });
+  return newSentence.join(" ")
+}
+
+
+// to add album art to name
+// data.albums.items[1].images[0].url
